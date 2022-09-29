2825140c3128243973b8e5707da83dcb8ebd599a
Mehmet Doganay <mehmetdoganay@Mehmet-MacBook-Air.local>
2022-09-29 17:13:09 +0300

    İstenilen kadar değer alan metod

--- methods2/src/Main.java
+++ methods2/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
         int toplam = topla(5,8);
         System.out.println(toplam);
 
+        int toplam2 = topla2(2,4,6,7,4,2,6,8,1231,1312,567,567);
+        System.out.println("Topla2 : "+ toplam2);
+
         String sehir = sehirGetir();
         System.out.println(sehir);
     }
@@ -23,6 +26,17 @@ public class Main {
     public static int topla(int num1, int num2) {
 
         return num1 + num2;
+    }
+
+    // int'ın yanına üç nokta (...) koyduğunda bu sayılar değişkenini bir diziye çevirir.
+    // Ve istediğiniz kadar değer verilebilir.
+    public static int topla2(int... sayilar){
+        int toplam = 0;
+        for (int sayi :sayilar)
+        {
+            toplam += sayi;
+        }
+        return toplam;
     }
 
     public static String sehirGetir()
